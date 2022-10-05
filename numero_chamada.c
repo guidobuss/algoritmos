f71f830bf9c135d4f9ff0399d270b556f2f37660
Guido Valmor Buss <101000466+guidobuss@users.noreply.github.com>
2022-10-05 20:34:00 -0300

    05/10

---
+++ numero_chamada.c
@@ -0,0 +1,21 @@
+/* 
+Estes algotirmos fazem parte dos exercícios
+ realizados e resolvidos em sala das turmas
+ 1A, 1B e 1C do C.E.Humberto de Alencar Castelo
+ Branco de Pinhais, das turmas de Pensamento
+ Computacional do Prof. Guido V. Buss 
+ */
+
+/*
+Exercício
+Escreva um algoritmo que utilize o valor 
+de seu número de chamada, mostrando na tela 
+a multiplicação deste número por 10.
+*/
+
+#include <stdio.h>
+
+void main(){
+    int chamada = 72;
+    printf("\n A multiplicação do meu número de chamada por 10 resulta em: %d \n",chamada*10);
+}
